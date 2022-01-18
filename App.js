2b0b327655c827bbea9abee00d0b9d181fac1d90
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2022-01-18 01:58:29 -0300

    naorema start

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import logo from './assets/logo.jpg'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>NAOREMA</Text>
+      <Image style={styles.logo} source={logo}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width:'5rem',
+    height:'5rem'
+  }
 });
